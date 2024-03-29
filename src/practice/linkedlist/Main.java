af2c8c072b15df3a58556c1a581a60badc74d4ae
SarveshSrivastava <ssarveshss2997@gmail.com>
2024-03-29 23:12:54 +0530

    printList and getter methods

--- src/practice/linkedlist/Main.java
+++ src/practice/linkedlist/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedList ll = new LinkedList(4);
+		
+		ll.getHead();
+		ll.getTail();
+		ll.getLength();
+		
+		ll.printList();
 	}
 
 }
